869d6ec17ffd7eae9b66273aecc920021c845a53
rdthetha <rd10072000@gmail.com>
2021-09-25 00:16:47 +0530

    Feat:feature

---
+++ app.js
@@ -0,0 +1,22 @@
+var btnTranslate= document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
+var serverurl= "https://api.funtranslations.com/translate/minion.json";
+
+
+function getTranslation(text){
+    return serverurl+ "?text="+text;
+}
+
+function errorHandler(error){
+    console.log("ERROR OCCURED"+ error);
+}
+function clickHandler(){
+    //outputDiv.innerText=txtInput.value;
+    var inputText=txtInput.value;;
+    fetch(getTranslation(inputText)).then(response=>response.json()).then(json=>outputDiv.innerText=(json.contents.translated)).catch(errorHandler);
+};
+
+btnTranslate.addEventListener("click",clickHandler);
+
+
